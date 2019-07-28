d8816f6669771824a3267cd8636a0e99cf8aea45
Rahul Kumar <nihalrahulsingh@gmail.com>
2019-07-29 02:03:09 +0530

    print comminr

--- app/src/main/java/com/example/testingforbitbucket/MainActivity.java
+++ app/src/main/java/com/example/testingforbitbucket/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.testingforbitbucket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String str = " hi Rahul how are you  i hope ypur aare fine as well as i am also fine";
+
+        Log.d( "onCreate: ",str);
     }
 }
